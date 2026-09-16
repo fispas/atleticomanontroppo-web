d4af818ecbcf492680471f5b749c5c20a1d8d0fb
fispas <36760479+fispas@users.noreply.github.com>
2026-09-16 18:56:41 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// Incolla qui la configurazione del tuo progetto Firebase:
-// console Firebase > Impostazioni progetto > Le tue app > App web > Configurazione SDK.
+// Configurazione del progetto Firebase "atletico-calcetto".
 // Questi valori sono pubblici per design: la protezione dei dati sta in firestore.rules.
 export const firebaseConfig = {
-  apiKey: 'INCOLLA_API_KEY',
-  authDomain: 'INCOLLA_PROGETTO.firebaseapp.com',
-  projectId: 'INCOLLA_PROGETTO',
-  storageBucket: 'INCOLLA_PROGETTO.appspot.com',
-  messagingSenderId: 'INCOLLA_SENDER_ID',
-  appId: 'INCOLLA_APP_ID'
+  apiKey: "AIzaSyDNfhCP0VCH9IVcKaPGc6c2I0GfX-ZJEIM",
+  authDomain: "atletico-calcetto.firebaseapp.com",
+  projectId: "atletico-calcetto",
+  storageBucket: "atletico-calcetto.firebasestorage.app",
+  messagingSenderId: "779404433964",
+  appId: "1:779404433964:web:2ff7f309e82ae33a220f89"
 };
